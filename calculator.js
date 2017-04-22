2de37a839639da43851cefd6e328de643a8bd65d
Jeremy Wicks <artfullu@gmail.com>
2017-04-22 15:54:06 -0400

    Pushing final

--- calculator.js
+++ calculator.js
@@ -11,11 +11,81 @@ Calculator.prototype.get = function(){
 return this.tokenStream.shift()
 }
 
+var TreeNode = function(name,...children){
+	this.name = name;
+	this.children = children;
+	
+
+}
+
+Calculator.prototype.parseExpression = function(){
+
+	var term = this.parseTerm();
+	var a = this.parseA();
+	
+	// var factor = this.parseFactor();
+
+	return new TreeNode('Expression',term,a)
+}
+
+Calculator.prototype.parseTerm = function(){
+	// var token = this.get();
+	var factor = this.parseFactor();
+	var b = this.parseB();
+	
+	return new TreeNode('Term',factor,b)
+
+}
+
+Calculator.prototype.parseA = function(){
+	var next = this.peek();
+
+	if(next && next.name === "ADD"){
+		this.get();
+		return new TreeNode("A","+",this.parseTerm(),this.parseA())
+	}
+	else if(next && next.name === "SUB"){
+		this.get();
+		return new TreeNode("A", "-", this.parseTerm(), this.parseA())
+	}
+
+	else return new TreeNode("A")
+};
+
+
+Calculator.prototype.parseB = function(){
+	var next = this.peek();
+
+	if(next && next.name === "MUL"){
+		this.get();
+		return new TreeNode("B","*",this.parseTerm(),this.parseA())
+	}
+	if(next && next.name === "DIV"){
+		this.get();
+		return new TreeNode("B", "/", this.parseTerm(), this.parseA())
+	}
+
+	else return new TreeNode("B")
+};
+
+Calculator.prototype.parseFactor = function(){
+	var next = this.peek()
+
+	if(next && next.name === 'LPAREN'){
+		this.get();
+		var exp = this.parseExpression();
+		this.get();
+		return new TreeNode("Factor","(",exp,")")
+	}
+	else if (next && next.name === 'NUMBER') return new TreeNode("Factor",this.get())
+
+}
+
 Calculator.prototype.lexer = function(input){
 	var tokenTypes = [
 	["NUMBER", /^\d+/],
 	["ADD", /^\+/],
-	["SUB", /^\=/],
+	["SUB", /^\-/],
 	["MUL", /^\*/],
 	["DIV",/^\//],
 	["LPAREN",/^\(/],
@@ -41,10 +111,37 @@ Calculator.prototype.lexer = function(input){
 				input = input.slice(1/*result[0].length*/)
 			}
 		});
-  if(match === false) throw Error('Unparseable token: ' + inputString)
+  if(match === false) throw Error('Unparseable token: ' + input)
 	}
 
 	
 
 	return tokens;
 }
+
+TreeNode.prototype.accept = function(visitor){
+	return visitor.visit(this)
+}
+
+function InfixVisitor() {
+
+  this.visit = function(node) {
+    switch(node.name) {
+      case "Expression":
+        return node.children[0].accept(this) + node.children[1].accept(this);
+        break;
+
+      case "A":
+        if(node.children.length > 0) {
+          return  node.children[0] + node.children[1].accept(this) + node.children[2].accept(this);
+        } else {
+          return "";
+        }
+        break;      
+      default:
+        break;
+    }
+  }
+}
+
+
